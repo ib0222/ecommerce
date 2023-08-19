520a4d6d833b113726cbf7c27f3c03b82ec4edf5
ib0222 <ibrahim.ismayilzade221@gmail.com>
2023-08-19 16:29:54 +0400

    first commit

---
+++ src/pages/shop/Shop.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Shop() {
+  return (
+    <div className='shop'>
+        <div>
+            <h1>ibotech shop</h1>
+        </div>
+        <div className='products'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Shop
