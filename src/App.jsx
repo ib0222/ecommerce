eab614d42d55d31b830e9fdbe1e8327a5bab902a
ib0222 <ibrahim.ismayilzade221@gmail.com>
2023-08-20 13:33:56 +0400

    doing some logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Shop from "./pages/shop/Shop";
 import Cart from "./pages/cart/Cart";
+import { ShopContextProvider } from "./context/ShopContext";
 function App() {
   return (
     <>
-      <Router>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<Shop/>}/>
-          <Route path="/cart" element={<Cart/>}/>
-        </Routes>
-      </Router>
+      <ShopContextProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Shop />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </Router>
+      </ShopContextProvider>
     </>
   );
 }
--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-
+import { PRODUCTS } from "../../products";
+import Product from './Product';
+import './shop.css'
 function Shop() {
   return (
     <div className='shop'>
@@ -7,7 +9,7 @@ function Shop() {
             <h1>ibotech shop</h1>
         </div>
         <div className='products'>
-            
+            {PRODUCTS.map((product) => <Product data={product} key={product.id}/>)} 
         </div>
     </div>
   )
